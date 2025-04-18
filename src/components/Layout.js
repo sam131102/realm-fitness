a094e3d95da156684441409a759d811bf3af2457
Sambhav Garg <78362105+sam131102@users.noreply.github.com>
2025-04-18 10:11:24 -0700

    updates

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -70,9 +70,6 @@ function Layout({ children }) {
                             <a href="https://www.instagram.com/therealmfitnesskelowna" target="_blank" rel="noopener noreferrer">
                                 <img src="/instagram.png" alt="Instagram" className="h-12 w-12 hover:scale-110 transition" />
                             </a>
-                            <a href="https://www.tiktok.com/@therealmfitnesskelowna" target="_blank" rel="noopener noreferrer">
-                                <img src="/Tiktok.png" alt="TikTok" className="h-12 w-12 hover:scale-110 transition" />
-                            </a>
                         </div>
                     </div>
                 </div>
